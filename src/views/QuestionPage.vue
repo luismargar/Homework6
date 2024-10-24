8e95a4914f2b71943a11d4c146595cc56cbef1a9
Luis Martinez <lmartin1@stu.southtexascollege.edu>
2024-10-24 16:04:04 -0500

    added score to question page

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -4,6 +4,7 @@ import useAPI from '@/composables/useAPI';
 import { useRoute } from 'vue-router';
 import BaseTitle from '@/components/BaseTitle.vue';
 import DifficultyChip from '@/components/DifficultyChip.vue';
+import MainScore from '@/components/MainScore.vue';
 
 const api = useAPI()
 const question = ref(null)
@@ -46,7 +47,7 @@ const shuffle = (array) => {
 
 <template>
   <div v-if="question" class="flex h-full w-full flex-col items-center gap-8 p-10">
-    <BaseTitle>{{ question.catetory }}</BaseTitle>
+    <BaseTitle>{{ question.catetory }} - <MainScore></MainScore> </BaseTitle>
     <!-- {{  question.question }} -->
 
     <div v-html="question.question" class="text-center text-2xl font-bold"></div>
